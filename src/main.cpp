c4f22032fbf81740da0a4aa4858f336724b7379d
jiarung yeh <jiarung.yeh@gmail.com>
2026-06-14 21:38:54 +0800

    feat: add BME680 + BH1750 sensors and WiFi, move creds to secrets.h
    - lib_deps: BH1750, Adafruit BME680 (pulls Unified Sensor + BusIO)
    - read BME680 (temp/hum/pressure/gas) over I2C; connect WiFi at boot
    - extract WiFi credentials to gitignored src/secrets.h
    - add src/secrets.h.example template + .gitignore entry

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,48 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <BH1750.h>
+#include <WiFi.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME680.h>
+#include "secrets.h"
+
+Adafruit_BME680 bme;
+BH1750 lightMeter;
 
 void setup() {
     Serial.begin(115200);
 
-    delay(1000);
+    Wire.begin(8, 9);
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 
-    Serial.println("ESP32-S3 Boot OK");
+    if (!bme.begin()) {
+        Serial.println("BME680 not found");
+        while (1);
+    }
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println();
+    Serial.println(WiFi.localIP());
+
+
+    Serial.println("Boot OK");
 }
 
 void loop() {
-    Serial.println("alive");
-    delay(1000);
+    if (bme.performReading()) {
+
+        Serial.printf(
+            "Temp=%.1fC Hum=%.1f%% Pressure=%.1fhPa Gas=%.1fkΩ\n",
+            bme.temperature,
+            bme.humidity,
+            bme.pressure / 100.0,
+            bme.gas_resistance / 1000.0
+        );
+    }
+
+    delay(3000);
 }
